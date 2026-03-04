6b5e0bffefdac1dfbf7e8d0ab7b4d5bcfae47714
jumana <jmanhtrfan@gmail.com>
2026-03-04 04:37:29 +0300

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Jumana on 15/09/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
